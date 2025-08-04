c9ac2d5025f6db9431826219613d2b09a9d616cb
Ante Primorac <ante@agilo.com>
2025-08-04 13:06:56 +0200

    Refactor Layout component: replace SafeAreaView with View and update styles for improved layout consistency

--- components/ui/Layout.tsx
+++ components/ui/Layout.tsx
@@ -1,19 +1,21 @@
 import { clx } from '@/utils/clx';
-import { ScrollView, ScrollViewProps } from 'react-native';
-import { SafeAreaView, SafeAreaViewProps } from 'react-native-safe-area-context';
+import { ScrollView, ScrollViewProps, View, ViewProps } from 'react-native';
 
-export const Layout: React.FC<SafeAreaViewProps> = ({ className, ...props }) => {
-  return <SafeAreaView className={clx('flex-1 px-4 pt-6 bg-white', className)} {...props} />;
+export const Layout: React.FC<ViewProps> = ({ className, ...props }) => {
+  return <View className={clx('flex-1 px-safe-offset-4 pt-safe-offset-6 bg-white', className)} {...props} />;
 };
 
 export const LayoutWithScroll: React.FC<ScrollViewProps> = (props) => {
   return (
-    <ScrollView
-      {...props}
-      className={clx('flex-1 bg-white', props.className)}
-      contentContainerClassName={clx('px-safe-offset-4 py-safe-offset-6', props.contentContainerClassName)}
-    >
-      {props.children}
-    </ScrollView>
+    <View className="flex-1 bg-white relative">
+      <View className="absolute top-0 left-0 right-0 pt-safe bg-white z-10" />
+      <ScrollView
+        {...props}
+        className={clx('flex-1', props.className)}
+        contentContainerClassName={clx('px-safe-offset-4 pt-safe-offset-6 pb-6', props.contentContainerClassName)}
+      >
+        {props.children}
+      </ScrollView>
+    </View>
   );
 };
