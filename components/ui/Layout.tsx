5818273f16f054f60a97130e6002202c61d7e9f5
Ante Primorac <ante@agilo.com>
2025-08-22 12:27:34 +0200

    Refactor LayoutWithKeyboardAvoidingScroll to simplify structure and remove platform-specific handling for improved readability

--- components/ui/Layout.tsx
+++ components/ui/Layout.tsx
@@ -1,6 +1,5 @@
 import { clx } from '@/utils/clx';
 import {
-  Platform,
   KeyboardAvoidingView as RNKeyboardAvoidingView,
   ScrollView,
   ScrollViewProps,
@@ -29,29 +28,15 @@ export const LayoutWithScroll: React.FC<ScrollViewProps> = (props) => {
 
 export const LayoutWithKeyboardAvoidingScroll: React.FC<ScrollViewProps> = (props) => {
   return (
-    <View className="relative flex-1 bg-white">
+    <RNKeyboardAvoidingView behavior="padding" className="relative flex-1 bg-white">
       <View className="pt-safe absolute left-0 right-0 top-0 z-10 bg-white" />
-      {Platform.OS === 'ios' ? (
-        <ScrollView
-          {...props}
-          className={clx('flex-1', props.className)}
-          contentContainerClassName={clx('px-safe-offset-4 pt-safe-offset-6 pb-6', props.contentContainerClassName)}
-        >
-          <RNKeyboardAvoidingView behavior="padding" className="flex-1">
-            {props.children}
-          </RNKeyboardAvoidingView>
-        </ScrollView>
-      ) : (
-        <RNKeyboardAvoidingView behavior="height" className="flex-1">
-          <ScrollView
-            {...props}
-            className={clx('flex-1', props.className)}
-            contentContainerClassName={clx('px-safe-offset-4 pt-safe-offset-6 pb-6', props.contentContainerClassName)}
-          >
-            {props.children}
-          </ScrollView>
-        </RNKeyboardAvoidingView>
-      )}
-    </View>
+      <ScrollView
+        {...props}
+        className={clx('flex-1', props.className)}
+        contentContainerClassName={clx('px-safe-offset-4 pt-safe-offset-6 pb-6', props.contentContainerClassName)}
+      >
+        {props.children}
+      </ScrollView>
+    </RNKeyboardAvoidingView>
   );
 };
